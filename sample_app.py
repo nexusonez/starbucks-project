1b87660b81955c53527487f4b5f39c450b0629e3
nexusonez <asonkamlol88@gmail.com>
2023-11-25 03:48:47 +0800

    Update sample_app.py

--- sample_app.py
+++ sample_app.py
@@ -10,31 +10,9 @@ def main():
 def contact():
     return render_template("contact.html")
 
-# @app.route("/login", methods=["GET", "POST"])
-# def login():
-#     if request.method == "POST":
-#         # Handle the login logic here
-#         username = request.form["username"]
-#         password = request.form["password"]
-
-#         # Use a cursor to interact with the database
-#         cursor = conn.cursor()
-
-#         # SQL query to check if the username and password match a record in the database
-#         sql = "SELECT * FROM users WHERE username = %s AND password = %s"
-#         cursor.execute(sql, (username, password))
-
-#         # Fetch one result
-#         result = cursor.fetchone()
-
-#         # Check if a matching record is found
-#         if result:
-#             return redirect(url_for("main"))  # Redirect to the home page on successful login
-#         else:
-#             error_message = "Invalid username or password"
-#             return render_template("login.php", error=error_message)
-
-#     return render_template("login.php")
+@app.route("/login")
+def login():
+    return render_template("login.php")
 
 @app.route("/sign-up")
 def signup():
